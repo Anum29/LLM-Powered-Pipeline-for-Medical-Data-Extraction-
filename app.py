f9d494ebe39fd39826bcca97a4d0e534ca6ea6b1
Anum Hassan <30297142+Anum29@users.noreply.github.com>
2023-09-08 08:06:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,18 @@
-! pip3 install -r requirements.txt
-
 import tempfile
 from PIL import Image
-
-# Import os to set API key
 import os
-# Import OpenAI as main LLM service
+
+
 from langchain.llms import OpenAI
 from langchain.embeddings import OpenAIEmbeddings
-# Bring in streamlit for UI/app interface
 import streamlit as st
-
-# Import PDF document loaders...there's other ones as well!
 from langchain.document_loaders import PyPDFLoader
-# Import chroma as the vector store 
 from langchain.vectorstores import Chroma
-
-# Import vector store stuff
 from langchain.agents.agent_toolkits import (
     create_vectorstore_agent,
     VectorStoreToolkit,
     VectorStoreInfo
 )
-
 
 # Set the title and subtitle of the app
 st.title('🦜🔗 PDF-Chat: Interact with Your PDFs in a Conversational Way')
@@ -36,64 +26,24 @@ st.image(image)
 st.subheader('Upload your pdf')
 uploaded_file = st.file_uploader('', type=(['pdf',"tsv","csv","txt","tab","xlsx","xls"]))
 
-temp_file_path = os.getcwd()
-while uploaded_file is None:
-    x = 1
-        
-if uploaded_file is not None:
-    # Save the uploaded file to a temporary location
-    temp_dir = tempfile.TemporaryDirectory()
-    temp_file_path = os.path.join(temp_dir.name, uploaded_file.name)
-    with open(temp_file_path, "wb") as temp_file:
+
+if uploaded_file:
+    # Read the uploaded PDF file and create a temporary file
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
         temp_file.write(uploaded_file.read())
 
-    st.write("Full path of the uploaded file:", temp_file_path)
+    # Display a text input box for the user to enter a question
+    user_question = st.text_input('Ask a question about the document:', '')
 
-# Set APIkey for OpenAI Service
-# Can sub this out for other LLM providers
-os.environ['OPENAI_API_KEY'] = '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'
+    if st.button('Get Answer'):
+        # Load the PDF document
+        pdf_loader = PyPDFLoader(file_path=temp_file.name)
+        pdf_text = pdf_loader.load()
 
-# Create instance of OpenAI LLM
-llm = OpenAI(temperature=0.1, verbose=True)
-embeddings = OpenAIEmbeddings()
 
-# Create and load PDF Loader
-loader = PyPDFLoader(temp_file_path)
-# Split pages from pdf 
-pages = loader.load_and_split()
+        # Display the answer to the user
+        st.write('Answer:')
+        st.write("This is the answer")
 
-# Load documents into vector database aka ChromaDB
-store = Chroma.from_documents(pages, embeddings, collection_name='Pdf')
-
-# Create vectorstore info object
-vectorstore_info = VectorStoreInfo(
-    name="Pdf",
-    description=" A pdf file to answer your questions",
-    vectorstore=store
-)
-# Convert the document store into a langchain toolkit
-toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
-
-# Add the toolkit to an end-to-end LC
-agent_executor = create_vectorstore_agent(
-    llm=llm,
-    toolkit=toolkit,
-    verbose=True
-)
-
-# Create a text input box for the user
-prompt = st.text_input('Input your prompt here')
-
-# If the user hits enter
-if prompt:
-    # Then pass the prompt to the LLM
-    response = agent_executor.run(prompt)
-    # ...and write it out to the screen
-    st.write(response)
-
-    # With a streamlit expander  
-    with st.expander('Document Similarity Search'):
-        # Find the relevant pages
-        search = store.similarity_search_with_score(prompt) 
-        # Write out the first 
-        st.write(search[0][0].page_content)
+        # Delete the temporary file
+        os.remove(temp_file.name)
